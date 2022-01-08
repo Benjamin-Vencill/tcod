d2850f123a4c8429c7284c6af5821effb52ab79f
Benjamin Vencill <btvencill@gmail.com>
2022-01-08 18:15:15 -0500

    Message Log and HP Bar

--- actions.py
+++ actions.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import Optional, Tuple, TYPE_CHECKING
 
+import color
+
 if TYPE_CHECKING:
     from engine import Engine
     from entity import Actor, Entity
@@ -96,16 +98,25 @@ class MeleeAttack(DirectionalAction):
         if not target:
             # No entity to attack
             return
-        
+
         damage = self.entity.fighter.power - target.fighter.defense
 
         attack_desc = f"{self.entity.name.capitalize()} attacks {target.name}"
 
+        if self.entity is self.engine.player:
+            attack_color = color.player_atk
+        else:
+            attack_color = color.enemy_atk
+
         if damage > 0:
             target.fighter.hp -= damage
-            print(f"{attack_desc} for {damage} hit points!")
+            self.engine.message_log.add_message(
+                f"{attack_desc} for {damage} hit points!", attack_color
+            )
         else:
-            print(f"{attack_desc} but does no damage.")
+            self.engine.message_log.add_message(
+                f"{attack_desc} but does no damage.", attack_color
+            )
 
 
 class BumpAction(DirectionalAction):
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import copy
 
 import tcod
 
+import color
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -11,7 +12,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
@@ -34,6 +35,9 @@ def main() -> None:
         engine=engine,
     )
     engine.update_fov()
+    engine.message_log.add_message(
+        "Hello and welcome adventurer!", color.welcome_text
+    )
 
 
 
